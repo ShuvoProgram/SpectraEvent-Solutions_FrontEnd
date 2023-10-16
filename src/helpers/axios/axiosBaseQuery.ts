5224e64cc681d6513f922b24ccd27d5b4701c034
Shuvo Khan <shuvoprogramer@gmail.com>
2023-10-16 19:13:23 +0600

    added axios configure

--- src/helpers/axios/axiosBaseQuery.ts
+++ src/helpers/axios/axiosBaseQuery.ts
@@ -1,6 +1,7 @@
 import { IMeta } from "@/types";
 import type { BaseQueryFn } from "@reduxjs/toolkit/query";
 import type { AxiosRequestConfig, AxiosError } from 'axios';
+import { instance as axiosInstance } from "./axiosInstance";
 
 export const axiosBaseQuery = (
   { baseUrl }: { baseUrl: string } = { baseUrl: "" }
@@ -17,8 +18,24 @@ export const axiosBaseQuery = (
   unknown
 > => async ({ url, method, data, params, contentType }) => {
   try {
-    const result = await axi
-  } catch (error) {
-
+    const result = await axiosInstance({
+      url: baseUrl + url,
+      method,
+      data,
+      params,
+      headers: {
+        "Content-Type": contentType || "application/json",
+      },
+      withCredentials: true,
+    })
+    return result;
+  } catch (axiosError) {
+    let err = axiosError as AxiosError;
+    return {
+      error: {
+        status: err.response?.status,
+        data: err.response?.data || err.message,
+      },
+    };
   }
 }
