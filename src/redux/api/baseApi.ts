3d6c0600615fc497b976fda37bdeafc27ad84c9c
Shuvo Khan <shuvoprogramer@gmail.com>
2023-10-16 19:04:40 +0600

    added redux setup

--- src/redux/api/baseApi.ts
+++ src/redux/api/baseApi.ts
@@ -1,6 +1,7 @@
 import { axiosBaseQuery } from "@/helpers/axios/axiosBaseQuery";
 import { createApi } from "@reduxjs/toolkit/query/react";
 import { tagTypesList } from "../tag-types";
+import { getBaseUrl } from "@/helpers/config/envConfig";
 
 export const baseApi = createApi({
   reducerPath: 'api',
