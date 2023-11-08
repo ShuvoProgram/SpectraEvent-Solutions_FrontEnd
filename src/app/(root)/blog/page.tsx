96eafadab08431a549af257a578fe547daee5ffb
ShuvoProgram <shuvoprogramer@gmail.com>
2023-11-08 21:37:07 +0600

    added blog page2

--- src/app/(root)/blog/page.tsx
+++ src/app/(root)/blog/page.tsx
@@ -3,6 +3,7 @@ import { Col, Row } from 'antd';
 import BlogCard from '@/components/Blog/BlogCard';
 import BreadCrumb from '@/components/shared/BreadCrumb';
 import { Metadata } from 'next';
+import Blog from '@/components/Blog/Blog';
 
 export const metadata: Metadata = {
   title: `SpectraEvent-Solutions | Blog`,
@@ -10,57 +11,10 @@ export const metadata: Metadata = {
 }
 
 function BlogPage() {
-  const items = {
-    label: 'Blog',
-    link: '/blog'
-  }
   return (
-    <div className='pt-10 px-4 pb-8'>
-     <div className='flex items-center justify-center'>
-     <BreadCrumb 
-      items={[
-        {
-          label: "Blog",
-          link: "/blog",
-        },
-      ]}
-      />
-     </div>
-      <div className="mt-5 grid grid-cols-1 lg:grid-cols-3 md:grid-cols-3 gap-4">
-      <BlogCard 
-      author='shuvo'
-       title='john' 
-       img='https://i.ibb.co/b36rWjT/1.png' 
-       date='12-2-23' description='test' 
-       liked={true} 
-       likeCount={12} 
-       link={`/`}/>
-       <BlogCard 
-      author='shuvo'
-       title='john' 
-       img='https://i.ibb.co/b36rWjT/1.png' 
-       date='12-2-23' description='test' 
-       liked={true} 
-       likeCount={12} 
-       link={`/`}/>
-       <BlogCard 
-      author='shuvo'
-       title='john' 
-       img='https://i.ibb.co/b36rWjT/1.png' 
-       date='12-2-23' description='test' 
-       liked={true} 
-       likeCount={12} 
-       link={`/`}/>
-       <BlogCard 
-      author='shuvo'
-       title='john' 
-       img='https://i.ibb.co/b36rWjT/1.png' 
-       date='12-2-23' description='test' 
-       liked={true} 
-       likeCount={12} 
-       link={`/`}/>
-        </div>
-    </div>
+   <>
+   <Blog/>
+   </>
   )
 }
 
---
+++ src/components/Blog/Blog.tsx
@@ -0,0 +1,69 @@
+"use client";
+import { useGetAllBlogQuery } from '@/redux/api/blogApi';
+import React from 'react'
+import Spinner from '../Loading/Spinner';
+import BreadCrumb from '../shared/BreadCrumb';
+import BlogCard from './BlogCard';
+import { Skeleton } from 'antd';
+
+function Blog() {
+    const query: Record<string, any> = {};
+  const {data, isLoading, isError} = useGetAllBlogQuery({...query})
+  if(isLoading) {
+    return <Spinner/>
+  }
+   // @ts-ignore
+   const blogs = data?.blog?.data;
+   // @ts-ignore
+  console.log(data?.blog?.data);
+  return (
+    <div className='container my-10'>
+        <h1
+        style={{
+          margin: "20px 0px",
+          color: "#FF5B22",
+          fontSize: "40px",
+          fontFamily: "serif",
+          fontWeight: "bold",
+          textAlign: "center"
+        }}
+      >Blog</h1>
+     <div className='flex items-center justify-center'>
+     <BreadCrumb 
+      items={[
+        {
+          label: "Blog",
+          link: "/blog",
+        },
+      ]}
+      />
+     </div>
+      <div className="mt-5 grid grid-cols-1 lg:grid-cols-3 md:grid-cols-3 gap-4">
+      {!isError ? (
+            blogs?.slice(0, 3)
+              .map((blog: any) => (
+                <div key={blog.id}>
+                  {!isLoading ? (
+                     <BlogCard 
+                     adminId={blog?.adminId}
+                      title={blog?.title} 
+                      img={blog?.image} 
+                      date={blog?.createdAt}
+                      description={blog?.content}
+                     contentType={blog?.contentType} 
+                      id={blog?.id}
+                      />
+                  ) : (
+                    <Skeleton avatar paragraph={{ rows: 4 }} />
+                  )}
+                </div>
+              ))
+          ) : (
+            <p>Failed to fetch event list.</p>
+          )}
+        </div>
+    </div>
+  )
+}
+
+export default Blog
